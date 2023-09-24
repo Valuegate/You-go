81c4d0022774e7e9e2643f18e4cb55b2c9c2cd84
semight <davidoluwasemiloorei@gmail.com>
2023-09-24 03:50:46 +0100

    notifcations page,settings page of buyerdashboard done with their components

--- src/app/buyerdashboard/notifications/page.tsx
+++ src/app/buyerdashboard/notifications/page.tsx
@@ -4,6 +4,10 @@ const Notifications = () => {
     return (
         <>
         <div className="pt-4">
+        <div className="flex justify-between pb-2">
+          <h2 className="text-base font-bold text-light-black-5">Notifications</h2>
+          <h3 className="text-sm font-bold text-light-black-5">Mark all as read</h3>
+        </div>
           <div className="bg-primary-1 rounded-lg px-4 py-4">
             <div className="flex flex-col gap-3">
                 <NotificationCard />
