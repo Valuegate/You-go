ad692141bababb55605f97e5c569e3888feb1285
semight <davidoluwasemiloorei@gmail.com>
2023-10-27 13:33:36 +0100

    fixing of footer text

--- src/components/Footer/page.tsx
+++ src/components/Footer/page.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
             <div className="bg-white w-[69px] h-[96px] md:w-[96px] md:h-[128px] flex items-center justify-center ">
               <Image src={logo} alt="Logo" className="w-[69px] h-[96px] md:w-[96px] md:h-[128px]" />
             </div>
-            <p className="text-white mt-3 font-normal text-base md:text-lg">
+            <p className="text-white mt-3 font-normal text-base">
               Kindly click on the social media icon below to follow us and know more about us. Thank you.
             </p>
 
@@ -33,22 +33,22 @@ const Footer = () => {
 
             <div className="flex w-full gap-4 mt-4">
               <Link href="#">
-                <span className="flex justify-center items-center rounded-full bg-primary w-[40px] h-[40px] md:w-[64px] md:h-[64px]">
+                <span className="flex justify-center items-center rounded-full bg-primary w-[40px] h-[40px]">
                   <FaLinkedinIn className="text-white" />
                 </span>
               </Link>
               <Link href="#">
-                <span className="flex justify-center items-center rounded-full bg-primary w-[40px] h-[40px] md:w-[64px] md:h-[64px]">
+                <span className="flex justify-center items-center rounded-full bg-primary w-[40px] h-[40px]">
                   <BiLogoInstagramAlt className="text-white" />
                 </span>
               </Link>
               <Link href="#">
-                <span className="flex justify-center items-center rounded-full bg-primary w-[40px] h-[40px] md:w-[64px] md:h-[64px]">
+                <span className="flex justify-center items-center rounded-full bg-primary w-[40px] h-[40px]">
                   <FaTwitter className="text-white" />
                 </span>
               </Link>
               <Link href="#">
-                <span className="flex justify-center items-center rounded-full bg-primary w-[40px] h-[40px] md:w-[64px] md:h-[64px]">
+                <span className="flex justify-center items-center rounded-full bg-primary w-[40px] h-[40px]">
                   <FaFacebookF className="text-white" />
                 </span>
               </Link>
@@ -56,42 +56,42 @@ const Footer = () => {
           </div>
           <div className="w-full md:w-[54%] mt-4 md:mt-0">
             <div className="flex flex-col md:flex-row justify-between">
-            <div className="flex flex-row md:flex-col gap-24 md:gap-16">
+            <div className="flex flex-row md:flex-col gap-24">
               <div>
-                <p className="text-white text-base mb-3 font-bold md:text-lg">
-                  Shop
+                <p className="text-white text-base mb-3 font-bold">
+                Product Categories
                 </p>
-                <p className="text-white font-normal text-base md:text-lg">Shop Us</p>
-                <p className="text-white font-normal text-base md:text-lg">Shop Us</p>
-                <p className="text-white font-normal text-base md:text-lg">Shop Us</p>
+                <p className="text-white font-normal text-base">Apparel</p>
+                <p className="text-white font-normal text-base">Footwear</p>
+                <p className="text-white font-normal text-base">Electronics</p>
               </div>
               <div className="">
-                <p className="text-white mb-3 font-bold md:text-lg">About</p>
-                <p className="text-white font-normal text-base md:text-lg">About Us</p>
-                <p className="text-white font-normal text-base md:text-lg">About Us</p>
-                <p className="text-white font-normal text-base md:text-lg">About Us</p>
+                <p className="text-white mb-3 font-bold text-base">About</p>
+                <p className="text-white font-normal text-base">Our Story</p>
+                <p className="text-white font-normal text-base">Meet the Team</p>
+                <p className="text-white font-normal text-base">Our Mission</p>
               </div>
             </div>
-            <div className="flex flex-row md:flex-col gap-20 md:gap-16 mt-4 md:mt-0">
+            <div className="flex flex-row md:flex-col gap-24 mt-4 md:mt-0">
               <div>
-                <p className="text-white mb-3 font-bold md:text-lg">Sell</p>
-                <p className="text-white font-normal text-base md:text-lg">Click to sell</p>
-                <p className="text-white font-normal text-base md:text-lg">Click to sell</p>
-                <p className="text-white font-normal text-base md:text-lg">Click to sell</p>
+                <p className="text-white mb-3 font-bold text-base">Sell</p>
+                <p className="text-white font-normal text-base">Home & Living</p>
+                <p className="text-white font-normal text-base">Beauty & Personal Care</p>
+                <p className="text-white font-normal text-base">Sports & Outdoors</p>
               </div>
               <div className="">
-                <p className="text-white mb-3 font-bold md:text-lg">Terms</p>
-                <p className="text-white font-normal text-base md:text-lg">Terms and Condition</p>
-                <p className="text-white font-normal text-base md:text-lg">Terms and Condition</p>
-                <p className="text-white font-normal text-base md:text-lg">Terms and Condition</p>
+                <p className="text-white mb-3 font-bold text-base">Terms</p>
+                <p className="text-white font-normal text-base">Disclaimer</p>
+                <p className="text-white font-normal text-base">Refund Policy</p>
+                <p className="text-white font-normal text-base">Cookies & Tracking</p>
               </div>
             </div>
             <div className="h-full mt-4 md:mt-0">
               <div>
-                <p className="text-white mb-3 font-bold md:text-lg">Help</p>
-                <p className="text-white font-normal text-base md:text-lg">Contact for help</p>
-                <p className="text-white font-normal text-base md:text-lg">Contact for help</p>
-                <p className="text-white font-normal text-base md:text-lg">Contact for help</p>
+                <p className="text-white mb-3 font-bold text-base">Help</p>
+                <p className="text-white font-normal text-base">Contact us</p>
+                <p className="text-white font-normal text-base">FAQ</p>
+                <p className="text-white font-normal text-base">Shipping & Delivery</p>
               </div>
             </div>
             </div>
